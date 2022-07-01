c4adaa2bd4038cc4536f4d80e907d9fdc97f40a2
Rinteger2022 <97272565+Rinteger2022@users.noreply.github.com>
2022-07-01 11:33:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,8 @@ DATA_URL = ("https://docs.google.com/spreadsheets/d/e/2PACX-1vRffwlmhiARpcl03gaG
 data = pd.read_csv(DATA_URL)
 #data = load_data()
 st.write(data)
- bar_chart = alt.Chart(data).mark_bar().encode(
+bar_chart = alt.Chart(data).mark_bar().encode(
         y='Inventory Quantity:Q',
         x='Title:O',
     )
- 
-    st.altair_chart(bar_chart, use_container_width=True)
+ st.altair_chart(bar_chart, use_container_width=True)
